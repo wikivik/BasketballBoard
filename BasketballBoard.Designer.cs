0c523f9ccfa27c5e8ec6f6239a801391a8529521
wikivik <wiktorkujawa92@gmail.com>
2018-06-05 17:16:56 +0200

    initial commit

---
+++ BasketballBoard.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace BasketballBoard
+{
+    partial class BasketballBoard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.team1Label = new System.Windows.Forms.Label();
+            this.team2Label = new System.Windows.Forms.Label();
+            this.pkt1Label = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.l5 = new System.Windows.Forms.Label();
+            this.pkt2Label = new System.Windows.Forms.Label();
+            this.l4 = new System.Windows.Forms.Label();
+            this.l3 = new System.Windows.Forms.Label();
+            this.l1 = new System.Windows.Forms.Label();
+            this.l2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // team1Label
+            // 
+            this.team1Label.AutoSize = true;
+            this.team1Label.Location = new System.Drawing.Point(78, 48);
+            this.team1Label.Name = "team1Label";
+            this.team1Label.Size = new System.Drawing.Size(35, 13);
+            this.team1Label.TabIndex = 0;
+            this.team1Label.Text = "label1";
+            // 
+            // team2Label
+            // 
+            this.team2Label.AutoSize = true;
+            this.team2Label.Location = new System.Drawing.Point(78, 73);
+            this.team2Label.Name = "team2Label";
+            this.team2Label.Size = new System.Drawing.Size(35, 13);
+            this.team2Label.TabIndex = 1;
+            this.team2Label.Text = "label2";
+            // 
+            // pkt1Label
+            // 
+            this.pkt1Label.AutoSize = true;
+            this.pkt1Label.Location = new System.Drawing.Point(78, 97);
+            this.pkt1Label.Name = "pkt1Label";
+            this.pkt1Label.Size = new System.Drawing.Size(35, 13);
+            this.pkt1Label.TabIndex = 2;
+            this.pkt1Label.Text = "label3";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(78, 131);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(35, 13);
+            this.timeLabel.TabIndex = 3;
+            this.timeLabel.Text = "label4";
+            // 
+            // l5
+            // 
+            this.l5.AutoSize = true;
+            this.l5.Location = new System.Drawing.Point(78, 169);
+            this.l5.Name = "l5";
+            this.l5.Size = new System.Drawing.Size(35, 13);
+            this.l5.TabIndex = 4;
+            this.l5.Text = "label5";
+            // 
+            // pkt2Label
+            // 
+            this.pkt2Label.AutoSize = true;
+            this.pkt2Label.Location = new System.Drawing.Point(174, 73);
+            this.pkt2Label.Name = "pkt2Label";
+            this.pkt2Label.Size = new System.Drawing.Size(35, 13);
+            this.pkt2Label.TabIndex = 5;
+            this.pkt2Label.Text = "label6";
+            // 
+            // l4
+            // 
+            this.l4.AutoSize = true;
+            this.l4.Location = new System.Drawing.Point(174, 48);
+            this.l4.Name = "l4";
+            this.l4.Size = new System.Drawing.Size(35, 13);
+            this.l4.TabIndex = 6;
+            this.l4.Text = "label7";
+            // 
+            // l3
+            // 
+            this.l3.AutoSize = true;
+            this.l3.Location = new System.Drawing.Point(174, 97);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(35, 13);
+            this.l3.TabIndex = 7;
+            this.l3.Text = "label8";
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.Location = new System.Drawing.Point(174, 131);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(35, 13);
+            this.l1.TabIndex = 8;
+            this.l1.Text = "label9";
+            // 
+            // l2
+            // 
+            this.l2.AutoSize = true;
+            this.l2.Location = new System.Drawing.Point(174, 169);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(41, 13);
+            this.l2.TabIndex = 9;
+            this.l2.Text = "label10";
+            // 
+            // BasketballBoard
+            // 
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.l1);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.l4);
+            this.Controls.Add(this.pkt2Label);
+            this.Controls.Add(this.l5);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.pkt1Label);
+            this.Controls.Add(this.team2Label);
+            this.Controls.Add(this.team1Label);
+            this.Name = "BasketballBoard";
+            this.Size = new System.Drawing.Size(546, 368);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Label team1Label;
+        private System.Windows.Forms.Label team2Label;
+        private System.Windows.Forms.Label pkt1Label;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label l5;
+        private System.Windows.Forms.Label pkt2Label;
+        private System.Windows.Forms.Label l4;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label l2;
+    }
+}
---
+++ BasketballBoard.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasketballBoard
+{
+    public partial class BasketballBoard : UserControl
+    {
+        public string Team1Name
+        {
+            get
+            {
+                return team1Label.Text;
+            }
+            set
+            {
+                team1Label.Text = value;
+            }
+        }
+        public Color Team1Color
+        {
+            get
+            {
+                return team1Label.ForeColor;
+            }
+            set
+            {
+                team1Label.ForeColor = value;
+            }
+        }
+        public string Team2Name
+        {
+            get
+            {
+                return team2Label.Text;
+            }
+            set
+            {
+                team2Label.Text = value;
+            }
+        }
+        public Color Team2Color
+        {
+            get
+            {
+                return team2Label.ForeColor;
+            }
+            set
+            {
+                team2Label.ForeColor = value;
+            }
+        }
+
+        public int PointsTeam1
+        {
+            get
+            {
+                try
+                {
+                return int.Parse(pkt1Label.Text);
+                }catch(Exception)
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                if (value / 10 == 0)
+                {
+                    pkt1Label.Text = "00" + value;
+                }
+                else if (value / 100 == 0)
+                {
+                    pkt1Label.Text = "0" + value;
+                }
+                else if (value / 1000 == 0)
+                {
+                    pkt1Label.Text = value.ToString();
+                }
+                else
+                {
+                    pkt1Label.Text = "ERR";
+                }
+
+            }
+        }
+        public Color Points1Color
+        {
+            get
+            {
+                return pkt1Label.ForeColor;
+            }
+            set
+            {
+                pkt1Label.ForeColor = value;
+            }
+        }
+        public int PointsTeam2
+        {
+            get
+            {
+                try
+                {
+                    return int.Parse(pkt2Label.Text);
+                }
+                catch (Exception)
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                if (value / 10 == 0)
+                {
+                    pkt2Label.Text = "00" + value;
+                }
+                else if (value / 100 == 0)
+                {
+                    pkt2Label.Text = "0" + value;
+                }
+                else if (value / 1000 == 0)
+                {
+                    pkt2Label.Text = value.ToString();
+                }
+                else
+                {
+                    pkt2Label.Text = "ERR";
+                }
+            }
+        }
+        public Color Point2Color
+        {
+            get
+            {
+                return pkt2Label.ForeColor;
+            }
+            set
+            {
+                pkt2Label.ForeColor = value;
+            }
+        }
+        public TimeSpan Time
+        {
+            get
+            {
+                return TimeSpan.Parse(timeLabel.Text);
+            }
+            set
+            {
+                timeLabel.Text = value.ToString();
+            }
+        }
+        public Color TimeColor
+        {
+            get
+            {
+                return timeLabel.ForeColor;
+            }
+            set
+            {
+                timeLabel.ForeColor = value;
+            }
+        }
+        public Color L1Color
+        {
+            get
+            {
+                return l1.ForeColor;
+            }
+            set
+            {
+                l1.ForeColor = value;
+            }
+        }
+        public Color L2Color
+        {
+            get
+            {
+                return l2.ForeColor;
+            }
+            set
+            {
+                l2.ForeColor = value;
+            }
+        }
+        public Color L3Color
+        {
+            get
+            {
+                return l3.ForeColor;
+            }
+            set
+            {
+                l3.ForeColor = value;
+            }
+        }
+        public Color L4Color
+        {
+            get
+            {
+                return l4.ForeColor;
+            }
+            set
+            {
+                l4.ForeColor = value;
+            }
+        }
+        public Color L5Color
+        {
+            get
+            {
+                return l5.ForeColor;
+            }
+            set
+            {
+                l5.ForeColor = value;
+            }
+        }
+
+        public BasketballBoard()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
